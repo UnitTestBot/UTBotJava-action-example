e25f4032e4d17e74f01560f3f022507339e85183
Nikita Stroganov <stroganov.n.36@gmail.com>
2022-07-04 15:14:48 +0300

    Initial commit

---
+++ src/main/java/com/github/utbot/Main.java
@@ -0,0 +1,21 @@
+package com.github.utbot;
+
+public class Main {
+
+    public boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length; ++i) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int square(int number) {
+        return multiply(number, number);
+    }
+
+    private static int multiply(int x, int y) {
+        return Math.multiplyExact(x, y);
+    }
+}
